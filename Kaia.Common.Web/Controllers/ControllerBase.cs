06e98f74d5063b73377dc3311cf640a463c1c390
Ken Keenan <ken@kaia.ie>
2017-11-12 14:11:19 +0000

    Adds controller with logging

---
+++ Kaia.Common.Web/Controllers/ControllerBase.cs
@@ -0,0 +1,60 @@
+﻿using NLog;
+using System.Web.Mvc;
+
+namespace Kaia.Common.Web.Controllers
+{
+    /// <summary>
+    /// Controller base class with some additional functionality (e.g., logging)
+    /// </summary>
+    public abstract class ControllerBase : Controller
+    {
+        private readonly ILogger _logger;
+
+        protected ILogger Logger { get { return _logger; } }
+
+        protected ControllerBase()
+        {
+            _logger = LogManager.GetLogger(GetType().FullName);
+        }
+
+
+        public ActionResult ViewOrPartial()
+        {
+            if (Request.IsAjaxRequest())
+            {
+                return PartialView();
+            }
+            return View();
+        }
+
+
+        public ActionResult ViewOrPartial(string viewName)
+        {
+            if (Request.IsAjaxRequest())
+            {
+                return PartialView(viewName);
+            }
+            return View(viewName);
+        }
+
+
+        public ActionResult ViewOrPartial(object model)
+        {
+            if (Request.IsAjaxRequest())
+            {
+                return PartialView(model);
+            }
+            return View(model);
+        }
+
+
+        public ActionResult ViewOrPartial(string viewName, object model)
+        {
+            if (Request.IsAjaxRequest())
+            {
+                return PartialView(viewName, model);
+            }
+            return View(viewName, model);
+        }
+    }
+}
